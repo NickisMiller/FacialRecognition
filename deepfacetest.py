0e4c694c58cfe660d2bffbdf68f85f616f39934f
Nick <millern9@students.westerntc.edu>
2022-03-30 12:23:52 -0500

    fixed merge issue

--- deepfacetest.py
+++ deepfacetest.py
@@ -9,5 +9,5 @@ from deepface import DeepFace
 #analysis = DeepFace.analyze(img_path = r"C:\Users\nickm\OneDrive\Pictures\Camera Roll\img5.PNG", actions = ["age", "gender", "emotion", "race"])
 #print(analysis)
 
-######see if face is in a collection of faces#########
+######see if face is in a collection of faces##########
 recognition = DeepFace.find(img_path = r"C:\Users\nickm\OneDrive\Pictures\Camera Roll\img5.PNG", db_path = “C:/Users/nickm/OneDrive/Pictures/Camera Roll/nick-face")
